64d2df6a14806bd4f63d071e90dcdc2b915532c3
Janko <jankojovicic351@gmail.com>
2023-05-12 12:29:11 +0200

    State managment login

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,20 +1,39 @@
 import React from "react"
 import axios from 'axios';
 import { useNavigate  } from 'react-router-dom';
-
-
+import ClipLoader from 'react-spinners/ClipLoader';
+import  Loader  from "../loader/Loader";
+import $ from 'jquery';
 export default function Auth(props) {
     var password = ""  
     let navigate = useNavigate();
 
+
   
     const handleClick = async () => {
+      $("#wrong").css("display", "none");
+
+      var loader = document.getElementById("loader");
+      loader.style.display = "block";
       await axios.get(process.env.REACT_APP_API_URL + `/Services/Device/?mode=loginUser&password=${password}&i=web`)
       .then(response => {
           if(response.data.Items[1].Name === "Error") {
-              alert("Nepravilno geslo")
+            setTimeout(function() { 
+              
+              
+              loader.style.display = "none";
+              $("#wrong").css("display", "block");
+
+          
+          
+          }, 2000);             
+
+              
+            
           } else {
-              navigate('/dashboard');
+              setTimeout(function() { navigate('/dashboard'); }, 2000);             
+
+
           }
       })
       .catch(error => {
@@ -25,12 +44,18 @@ export default function Auth(props) {
         password = e.target.value;
     }
   return (
+    <div>
+
+
+    <Loader />
+
+
     <div className="Auth-form-container">
   
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Prijava</h3>
           
-          <div className="form-group mt-3">
+          <div className="form-group mt-3" id="password-div">
             <label>Password</label>
             <input
               id = "password"
@@ -39,7 +64,11 @@ export default function Auth(props) {
               className="form-control mt-1"
               placeholder="Enter password"
             />
+            
           </div>
+          <div class="alert alert-danger wrong" id="wrong" role="alert">
+        Napačno geslo.
+           </div>
           <div className="d-grid gap-2 mt-3">
             <button className="btn btn-primary" onClick={handleClick}>
               Submit
@@ -48,6 +77,7 @@ export default function Auth(props) {
           
         </div>
    
+    </div>
     </div>
   )
 }
---
+++ src/loader/Loader.js
@@ -0,0 +1,9 @@
+const Loader = ({ tableData, columns }) => {
+    return (
+        <img src="/loader.gif" id="loader" alt="loading" className="loader" />
+
+    );
+   };
+
+
+   export default Loader;
