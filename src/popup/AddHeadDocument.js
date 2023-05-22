0c6a39bfdad2a5d45cc15af4a0ad0e437a595293
Janko Jovičić <jankojovicic351@gmail.com>
2023-05-22 11:28:39 +0200

    Conditional component rendering adding new head element

--- src/popup/AddHeadDocument.js
+++ src/popup/AddHeadDocument.js
@@ -1,9 +1,35 @@
 
 import $ from 'jquery'; 
-
+import { useEffect, useState } from "react";
+import Select from 'react-select'
 
 export default function AddHeadDocument(props) { 
 
+
+
+
+    const  [documentType, setDocumentType] = useState([]);
+    const [type, setType] = useState();
+
+    useEffect(() => {
+        setDocumentType([{value: 'Izdaja blaga', label: 'Izdaja blaga'}, {value: 'Prevzem blaga', label: 'Prevzem blaga'},{value: 'Medskladišnica', label: 'Medskladišnica'},{value: 'Delovni nalog', label: 'Delovni nalog'},{value: 'Inventura', label: 'Inventura'}]);
+
+
+        alert(type.value)
+        switch (type.value) {  
+
+
+        }
+
+
+
+    }, [type]);
+
+
+    function onChangeTypePopup(e) {
+        const mutated = {value: e.value, label:e.label}
+        setType (mutated);   
+    }
     
     if(props.show) {
         $("#addHeader").css("display", "block");
@@ -23,78 +49,30 @@ export default function AddHeadDocument(props) {
 
 
         <div class="body_part">
-
         <div class="container py-5">
+
+
+
+
+
+            
     <div class="row">
         <div class="col-md-3 mx-auto">
 
                 <div class="form-group row">
-                                <label for="transactionId">Transakcija</label>
-                                <input type="text" class="form-control" id="transactionIdAdd" disabled />
-                </div>
-                <div class="form-group row">
-                                <label for="inputFirstname">Tip</label>
-                                <input type="text" class="form-control" id="typeAdd" disabled/>
-                </div>
-                <div class="form-group row">
-                                <label for="inputFirstname">Posl. Dog.</label>
-                                <input type="text" class="form-control" id="documentTypeAdd" disabled />
-                </div>
-                <div class="form-group row">
-                                <label for="inputFirstname">Naročnik</label>
-                                <input type="text" class="form-control" id="clientAdd" disabled />
-                </div>
-                <div class="form-group row">
-                                <label for="inputFirstname">Skladišče</label>
-                                <input type="text" class="form-control" id="warehouseAdd" disabled />
-                </div>
-
-
-                <div class="form-group row">
-                    <div class="col-sm-6">
-                        <label for="inputFirstname">Ident</label>
-                        <input type="text" class="form-control" id="ident" placeholder="Ident" />
-                    </div>
-                    <div class="col-sm-6">
-                        <label>Podatki o naročilo</label>
-
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <div class="col-sm-6">
-                        <label for="inputAddressLine1">Naziv</label>
-                        <input type="text" class="form-control" id="identName" placeholder="Street Address" />
-                    </div>
-                    <div class="col-sm-6">
-                        <label for="inputAddressLine2">Količina</label>
-                        <input type="text" class="form-control" id="inputAddressLine2" placeholder="Line 2" />
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <div class="col-sm-6">
-                        <label for="inputCity">City</label>
-                        <input type="text" class="form-control" id="inputCity" placeholder="City" />
-                    </div>
-                    <div class="col-sm-6">
-                        <label for="inputState">State</label>
-                        <input type="text" class="form-control" id="inputState" placeholder="State" />
-                    </div>
+                <Select className='select-filters' onChange={onChangeTypePopup}  placeholder={"Tip transakcije"} options = {documentType} id='transactionTypePopup'/>
 
                 </div>
-                <div class="form-group row">
-                    <div class="col-sm-6">
-                        <label for="inputContactNumber">Contact Number</label>
-                        <input type="number" class="form-control" id="inputContactNumber" placeholder="Contact Number" />
-                    </div>
-                    <div class="col-sm-6">
-                        <label for="inputWebsite">Website</label>
-                        <input type="text" class="form-control" id="inputWebsite" placeholder="Website" />
-                    </div>
-                </div>
-                <button type="button" class="btn btn-primary px-4 float-right">Save</button>
+                
+             
 
         </div>
     </div>
+
+
+
+
+
 </div>
 
 
