dc32d0d0bdb43c6502e9329da12bb7293d0da78e
Janko Jovičić <jankojovicic351@gmail.com>
2023-05-22 15:07:27 +0200

    Landing page

---
+++ src/homepage/Landing.js
@@ -0,0 +1,26 @@
+
+import $ from 'jquery'; 
+import Select from 'react-select'
+
+
+export default function TakeOver(props) { 
+
+
+     
+      
+
+
+  
+    return ( 
+
+
+
+        <div>
+      
+        </div>
+
+
+        ); 
+}
+
+
--- src/popup/WorkOrder.js
+++ src/popup/WorkOrder.js
@@ -3,37 +3,24 @@ import $ from 'jquery';
 import Select from 'react-select'
 
 
-export default function Add(props) { 
-
-
-
-
-
+export default function WorkOrder(props) { 
 
     var elms = data.push.props
     if(props.show) {
-
         $("#edit").css("display", "block");
-
     } else {
-
         $("#edit").css("display", "none");
-
     }
-  
+
     return ( 
 
     <div className='add-container-workorder'> 
-
 
 
        <div className='first-row'> 
             <Select className='select-filters' placeholder={"Tip transakcije"} id='documentType'/>
             <input type="text" id="example" class="form-control" /> 
        </div>
-
-
-
 
 
        <input type="text" id="example" class="form-control" />
