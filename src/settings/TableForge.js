455f446b5b4e2b9b2ab9cefc820f363b4ea0573f
Janko Jovičić <jankojovicic351@gmail.com>
2024-01-18 08:42:02 +0100

    Search and pagination component

--- src/settings/TableForge.js
+++ src/settings/TableForge.js
@@ -2,16 +2,18 @@ import React, { useState, useMemo } from 'react';
 import { MdEdit } from 'react-icons/md';
 import { IoAddCircleSharp } from 'react-icons/io5';
 import { MdDeleteForever } from 'react-icons/md';
-import { useTable, useGlobalFilter } from 'react-table';
+import { useTable, useGlobalFilter, usePagination } from 'react-table';
 import Swal from 'sweetalert2';
 import SettingsService from '../services/SettingsService';
 import Insert from '../popup/Insert';
 import Update from '../popup/Update';
+import { FaSearch } from 'react-icons/fa'; // Import the search icon
 
 function TableForge({ refresh, name, tableData }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isModalEditOpen, setIsEditModalOpen] = useState(false);
   const [editData, setEditData] = useState({});
+  const [isInputVisible, setIsInputVisible] = useState(false);
 
   const showDeleteConfirmation = (data) => {
     var currentDeleteSQL = selectedTable.deleteQuery;
@@ -105,7 +107,9 @@ function TableForge({ refresh, name, tableData }) {
           <button className="action-buttons white" title="Vnos" onClick={onAdd}>
             <IoAddCircleSharp />
             Dodaj
+            
           </button>
+          
         ),
         accessor: 'actions',
 
@@ -141,33 +145,47 @@ function TableForge({ refresh, name, tableData }) {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    page, // Instead of 'rows', we use 'page' which represents the currently visible page
     prepareRow,
-    state,
+    state: { globalFilter, pageIndex, pageSize },
     setGlobalFilter,
+    gotoPage,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    pageCount,
+    setPageSize,
   } = useTable(
     {
       columns: selectedTable ? selectedTable.value : [],
       data: tableData,
     },
-    useGlobalFilter
+    useGlobalFilter,
+    usePagination // Add the usePagination hook
   );
 
-  const { globalFilter } = state;
-
   return (
-    <div>
+    <div className='global-react-table'>
+      
+
+
+
+
+
       <Insert refresh={refresh} onClose={onClose} selectedTable={selectedTable} isVisible={isModalOpen} />
       <Update data={editData} refresh={refresh} onClose={onCloseEdit} selectedTable={selectedTable} isVisible={isModalEditOpen} />
-      <input
-          type="text"
-          placeholder="Iskanje..."
-          value={globalFilter || ''}
-          onChange={(e) => setGlobalFilter(e.target.value)}
-        />
-      <div className="user-settings-table">
-        
 
+
+
+
+
+
+   
+
+
+     <div className="user-settings-table">
         <table {...getTableProps()} className={`react-table-${name}`}>
           <thead>
             {headerGroups.map((headerGroup) => (
@@ -179,7 +197,7 @@ function TableForge({ refresh, name, tableData }) {
             ))}
           </thead>
           <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
+            {page.map((row) => {
               prepareRow(row);
               return (
                 <tr {...row.getRowProps()}>
@@ -192,6 +210,59 @@ function TableForge({ refresh, name, tableData }) {
           </tbody>
         </table>
       </div>
+
+ {/* Pagination controls */}
+ <div className='paginationControls'>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+
+
+
+        <div className="search-container">
+      <div className="search-icon" >
+        <FaSearch style={{ color: 'white', display: 'flex', alignItems: 'center' }} />
+      </div>
+
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search..."
+          value={globalFilter || ''}
+          onChange={(e) => setGlobalFilter(e.target.value)}
+          style={{
+            backgroundColor: '#081A45',
+            color: 'white',
+            border: 'none', // Remove the white border
+            borderRadius: '5px',
+            height: '2em',
+            padding: '10px',
+            transition: 'opacity 2.5s', // Apply a transition for a smooth appearance
+          }}
+        />
+
+    </div>
+
+
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Stran{' '}
+          <strong>
+            {pageIndex + 1} od {pageOptions.length}
+          </strong>{' '}
+        </span>
+    
+       
+      </div>
+
     </div>
   );
 }
