e917be891a67ac040d6facb8c19d75523d8b55e5
Janko Jovičić <jankojovicic351@gmail.com>
2024-01-19 13:19:22 +0100

    Sentry implementation.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
 
 import { useDispatch, useSelector } from 'react-redux'
 import {store} from './store/store';
@@ -10,6 +11,25 @@ import {store} from './store/store';
 import { Provider } from 'react-redux'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+
+Sentry.init({
+  dsn: "https://6ac4902002c955c3d29c9e2e8a78c01b@o4505147932344320.ingest.sentry.io/4506597835735040",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 
 
